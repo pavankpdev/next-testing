350538860d92a47cab0d4937ab840b5151ceea55
pavanleton <pavankumarkpkp@gmail.com>
2023-11-15 14:20:43 +0530

    test the band toute generated on demand

--- base-concert-venue/cypress/integration/routes.test.js
+++ base-concert-venue/cypress/integration/routes.test.js
@@ -1,3 +1,6 @@
+import {generateNewBand} from "../../__tests__/__mocks__/fakeData/newBand";
+import {generateRandomId} from "../../lib/features/reservations/utils";
+
 it("Displays correct heading when navigating to shows route", () => {
     cy.visit("/")
     cy.findByRole('button', {name: /shows/i}).click()
@@ -18,4 +21,11 @@ it("displays correct band name for band route that existed at build time", () =>
 it("displays band not found for the band route that does not exist", () => {
     cy.task("db:reset").visit("/bands/12333")
     cy.findByRole("heading", {name: /error: band not found/i}).should("exist")
+})
+
+it("Displays name for the band that was not present at build time", () => {
+    const bandId = generateRandomId()
+    const newBand = generateNewBand(bandId)
+    cy.task("db:reset").task("addBand", newBand).visit(`/bands/${bandId}`)
+    cy.findByRole("heading", {name: /Avalanche of Cheese/i}).should("exist")
 })
--- base-concert-venue/cypress/plugins/index.js
+++ base-concert-venue/cypress/plugins/index.js
@@ -13,6 +13,7 @@
 // the project's config changing)
 
 const {resetDb} = require("../../__tests__/__mocks__/db/utils/reset-db");
+const {addBand} = require("../../lib/features/bands/queries");
 /**
  * @type {Cypress.PluginConfig}
  */
@@ -22,5 +23,6 @@ module.exports = (on, config) => {
   // `config` is the resolved Cypress config
     on('task', {
         "db:reset": () => resetDb().then(() => null),
+        addBand: (newBand) => addBand(newBand).then(() => null)
     })
 }

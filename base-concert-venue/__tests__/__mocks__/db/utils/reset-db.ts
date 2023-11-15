aeebc7d5122ce2b8f9714a559fbafb9f6dcd9744
pavanleton <pavankumarkpkp@gmail.com>
2023-11-15 12:46:14 +0530

    add test local DB using JSON files

---
+++ base-concert-venue/__tests__/__mocks__/db/utils/reset-db.ts
@@ -0,0 +1,16 @@
+import {readFakeData} from "@/__tests__/__mocks__/fakeData"
+import {writeJSONToFile, filenames} from "@/lib/db/db-utils";
+
+export const resetDb = async (): Promise<void> => {
+    const safeToReset = process.env.NODE_ENV === "test";
+    if(!safeToReset) {
+        throw new Error("Not safe to reset database in non-test environment");
+    }
+    const fakeData = await readFakeData();
+    await Promise.any([
+        writeJSONToFile(filenames.users, fakeData.fakeUsers),
+        writeJSONToFile(filenames.shows, fakeData.fakeShows),
+        writeJSONToFile(filenames.bands, fakeData.fakeBands),
+        writeJSONToFile(filenames.reservations, fakeData.fakeReservations),
+    ])
+}
--- base-concert-venue/lib/db/constants.ts
+++ base-concert-venue/lib/db/constants.ts
@@ -1,4 +1,12 @@
+import * as process from "process";
+
 export const venueCapacity = 400;
 
 // this will eventually use environment variables
-export const getDbPath = (): string => "db";
+export const getDbPath = (): string | void => {
+    if(!process.env.DB_PATH) {
+        throw new Error("DB_PATH environment variable not set");
+    }
+
+    return process.env.DB_PATH
+}
